9fec99969c71afe0f54348a1b6db136cedbc16ad
tjaiswal <tjaiswal@tjaiswal-VAIO>
2017-10-21 11:31:42 +0530

    initails

---
+++ src/main/java/miscellaneous/com/concept1/App.java
@@ -0,0 +1,13 @@
+package miscellaneous.com.concept1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
